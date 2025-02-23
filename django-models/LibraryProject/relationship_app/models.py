14161f59301a5fc0a49bcf547ae3ec4a2f106ca1
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-02-23 12:59:52 +0200

    fix style issue

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Author (models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
