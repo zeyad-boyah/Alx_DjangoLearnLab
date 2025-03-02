e5c562e3c7f460b5fb0fa3259ad1c9984ed999ec
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-02 22:19:43 +0200

    add permissions to bookshelf

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -1,3 +1,19 @@
-# the GREATER LIBRARY 
+Custom Permissions Setup:
 
+Model: Book
+Custom Permissions:
+  - can_view: Allows viewing of Book instances.
+  - can_create: Allows creation of Book instances.
+  - can_edit: Allows editing of Book instances.
+  - can_delete: Allows deletion of Book instances.
 
+Groups:
+  - Editors: Assigned can_create and can_edit.
+  - Viewers: Assigned can_view.
+  - Admins: Assigned all permissions.
+
+Usage in Views:
+  Use the @permission_required decorator to enforce permissions. For example:
+    @permission_required('bookshelf.can_edit', raise_exception=True)
+    def edit_Book(request, pk):
+        ...
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,39 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+from bookshelf.models import Book
+
+def index (request):
+    return HttpResponse('welcome to bookshelf')
 
 
-def index(request):
-    return HttpResponse("Welcome to the Bookshelf app!")
+def setup_groups():
+    content_type = ContentType.objects.get_for_model(Book)
+    
+    # Get custom permissions
+    can_view = Permission.objects.get(codename='can_view', content_type=content_type)
+    can_create = Permission.objects.get(codename='can_create', content_type=content_type)
+    can_edit = Permission.objects.get(codename='can_edit', content_type=content_type)
+    can_delete = Permission.objects.get(codename='can_delete', content_type=content_type)
+
+    # Create groups if they don't already exist
+    editors, _ = Group.objects.get_or_create(name='Editors')
+    viewers, _ = Group.objects.get_or_create(name='Viewers')
+    admins, _ = Group.objects.get_or_create(name='Admins')
+
+    # Assign permissions to each group
+    editors.permissions.set([can_create, can_edit])
+    viewers.permissions.set([can_view])
+    admins.permissions.set([can_view, can_create, can_edit, can_delete])
+
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_mymodel(request, pk):
+    instance = get_object_or_404(Book, pk=pk)
+    
+    if request.method == 'POST':
+        return HttpResponse('you did the post request')
+    
+    return HttpResponse("didn't work")
