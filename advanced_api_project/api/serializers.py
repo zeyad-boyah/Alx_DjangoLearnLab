875b3029f6cc729b45a740227819bd5a6951420e
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-16 20:52:11 +0200

    add author serializer

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from api.models import Book, Author
 from datetime import datetime
 
@@ -9,10 +8,15 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = "__all__"
     
-    def validate(self, attrs):
-        publication_year = attrs.get('publication_year')
+    def validate(self, data):
+        publication_year = data.get('publication_year')
         current_year = datetime.now().year
         if publication_year and publication_year > current_year:
-            raise ValidationError("publication_year can't be in the future")
-        return attrs
+            raise serializers.ValidationError("publication_year can't be in the future")
+        return data
     
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True)
+    class Meta:
+        model = Author
+        fields = ["name", "books"]
