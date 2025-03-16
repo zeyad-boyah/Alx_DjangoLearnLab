13fecb6ebebdd312ebe28463a500c02d5e3c8788
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-16 22:58:53 +0200

    add orderingFilter to booklistview

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import generics
 from api.models import Book, Author
 from api.serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from django_filters import rest_framework
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 # api to list all books
@@ -12,11 +12,13 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
     # For filtering by publication_year or other exact matches:
     filterset_fields = ['publication_year']
     # For text searches on title and the related author's name:
     search_fields = ['title', 'author__name']
+    # allow ordering by title and publication_year
+    ordering_fields = ['title', 'publication_year']  
 
 # to look up a specific book with primary key in the slug like /books/pk/
 class BookDetailView(generics.RetrieveAPIView):
