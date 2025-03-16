7727cfd9c2f431c682f83f2269a60daa8e59ebf8
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-16 21:49:40 +0200

    add all CRUD opperations to Book model

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from api import views
 
 
 urlpatterns = [
-    
+    path("books/", views.BookListView.as_view(), name = "all books"),
+    path("books/<int:pk>/", views.BookDetailView.as_view(), name = "Lookup a book"),
+    path("books/<int:pk>/create/", views.BookCreateView.as_view(), name = "Create a book"),
+    path("books/<int:pk>/delete/", views.BookDeleteView.as_view(), name = "Delete a book"),
+    path("books/<int:pk>/update/", views.BookUpdateView.as_view(), name = "Update a book"),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from api.models import Book, Author
+from api.serializers import BookSerializer, AuthorSerializer
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# api to list all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# to look up a specific book with primary key in the slug like /books/pk/
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "pk"
+
+# to create a book with a post request while logged in 
+class BookCreateView(LoginRequiredMixin, generics.CreateAPIView ):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "pk"
+
+# to update a book with a Put request while logged in 
+class BookUpdateView(LoginRequiredMixin, generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "pk"
+
+# to delete  a book with pk while logged 
+class BookDeleteView(LoginRequiredMixin, generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "pk"
