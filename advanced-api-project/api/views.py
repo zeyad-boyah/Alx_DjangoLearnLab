ca12c973ebe69ae6d1bb41ae70fc3479c7f2aa8c
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-16 22:00:38 +0200

    test

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,37 +2,39 @@ from django.shortcuts import render
 from rest_framework import generics
 from api.models import Book, Author
 from api.serializers import BookSerializer, AuthorSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # api to list all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # to look up a specific book with primary key in the slug like /books/pk/
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = "pk"
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # to create a book with a post request while logged in 
 class BookCreateView(generics.CreateAPIView ):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = "pk"
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
 # to update a book with a Put request while logged in 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = "pk"
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
 # to delete  a book with pk while logged 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = "pk"
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
