5ebd7e9766d5cea48d56e0c3bbdffcc79196ad28
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-23 22:43:22 +0200

    add templates for update/delete/create

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -44,7 +44,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-created_at']  # to show the newest posts first
+    ordering = ['-published_date']  # to show the newest posts first
 
 # Display details for a single blog post
 class PostDetailView(DetailView):
