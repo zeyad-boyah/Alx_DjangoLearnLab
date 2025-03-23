dafae2a5a70c7d41e7621d97fccc74e97d9537d9
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-23 23:45:33 +0200

    add tag model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
 
     def __str__(self):
         return self.title
@@ -28,3 +34,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    
