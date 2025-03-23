6dd1e8a5d36ca59b717071d4709c5f89920cc9d6
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-23 21:47:11 +0200

    change database to mysql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Blogs',
+        'USER': 'root',
+        'PASSWORD': 'Github_password_69',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
