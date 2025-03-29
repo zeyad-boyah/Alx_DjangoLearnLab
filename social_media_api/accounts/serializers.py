99ca0232eb73c357e99fbe250a49b55614731400
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-29 03:55:39 +0200

    add registration view

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, password_validation
+
+User = get_user_model()
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators= [password_validation.validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+
+
+    """
+    write_only=True: Ensures that passwords are not exposed in API responses aka INPUT ONLY.
+    required=True: Makes both fields mandatory.
+    validators=[password_validation.validate_password]: Applies Django's built-in password strength validation.
+    """
+    def validate(self, data):
+        if data.get("password") != data.get("password2"):
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return data
+    
+    # remove password2 as it is no longer needed
+    def create(self, validated_data):
+        validated_data.pop("password2")  # Remove the password confirmation field
+        user = User.objects.create_user(**validated_data)  # Create user
+        return user
+
+
+
+
+class LoginSerializer(serializers.Serializer):
+    # use email for authentication
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True)
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from .serializers import RegistrationSerializer
 
-# Create your views here.
+
+User = get_user_model ()
+
+class RegistrationAPIView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        # get serializer and use it on the request data
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # call create() method in the serializer and save the user
+        user = serializer.save()
+        # provide a token for the new user
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({
+            "user": serializer.data,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+
+
+
