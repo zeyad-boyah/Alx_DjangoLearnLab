3d1bfbd4e6557f06df2f91ac3e6e838f089f4422
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-30 06:48:14 +0200

    update customuser to include following

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     bio = models.TextField(blank=True)
     profile_picture =models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField("self",symmetrical=True)
+    following = models.ManyToManyField("self",symmetrical=False, related_name="followers", blank=True)
 
     def __str__(self):
         return self.username
