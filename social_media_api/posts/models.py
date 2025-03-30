bc2b9426d7196d0bff3727cb4fdc5d8639d5dfc3
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-30 20:13:49 +0200

    commit after merging notification

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -25,3 +25,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"comment {self.pk} by {self.author}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
