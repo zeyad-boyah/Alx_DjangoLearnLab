08f9b6c8da6daa30e6b3f4fae5be4279cf9d740f
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-30 02:09:26 +0200

    add myposts &post R/U/D view

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts/", views.PostListAPIView.as_view(), name="List Post"),
-    path("post/create/", views.PostCreateAPIView.as_view(), name="create Post"),
+    path("posts/", views.PostListAPIView.as_view(), name="list post"),
+    path("post/create/", views.PostCreateAPIView.as_view(), name="create post"),
+    path("my_posts/", views.MyPostListAPIView.as_view(), name="my posts"),
+    path("my_post/retrieve/<int:pk>/", views.PostRetrieveUpdateDestroyAPIView.as_view(), name="my specific post retrieve"),
+    path("my_post/update/<int:pk>/", views.PostRetrieveUpdateDestroyAPIView.as_view(), name="my specific post Update"),
+    path("my_post/delete/<int:pk>/", views.PostRetrieveUpdateDestroyAPIView.as_view(), name="my specific post Delete"),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -5,15 +5,40 @@ from rest_framework import generics, permissions
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-
+# list all posts including their comments
 class PostListAPIView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostReadSerializer
 
+# list all posts made by the authenticated user
+class MyPostListAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostReadSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
+
+# create a Post with a login token
 class PostCreateAPIView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostWriteSerializer
     permission_classes = [permissions.IsAuthenticated]
-
+    # make it so that the author is the owner of the token
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+# while authorized retrieve a post made by the user, update or delete them
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostWriteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
+
